7330d56137b2a13283f927a0ea3a5ca0a6078f9b
khalmam <genkhal25@gmail.com>
2026-06-23 06:08:00 +0100

    Added facility clustering

---
+++ src/services/query/fetchFacilityClusters.service.js
@@ -0,0 +1,13 @@
+import { publicInstanceAxios } from "@config/axiosInstance";
+
+/**
+ * @desc fetches facility clusters or individual facilities for map display
+ * @param {Object} params - either { bbox, zoom, ...filters } for browse mode
+ *                           or { lat, lng, zoom, radius_km, ...filters } for nearby mode
+ * @returns {Object} { type: 'clusters' | 'facilities', count, results }
+ */
+export const fetchFacilityClusters = async (params) => {
+  const query = new URLSearchParams(params).toString();
+  const response = await publicInstanceAxios.get(`/facilities/clusters/?${query}`);
+  return response?.data;
+};
